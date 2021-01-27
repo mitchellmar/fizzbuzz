496678ce611985bc2b919db3ec3ec9df5c4db1fd
mitchellmar <mitchell_k_mar@yahoo.com>
2021-01-26 18:54:57 -0800

    refactor

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -1,21 +1,21 @@
 
 #include <iostream>
+using namespace std;
 
 int main() {
     for (int i = 1; i <= 100; ++i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            std::cout << "FizzBuzz" << "\n";
-            continue;
+
+        if (i % 15 == 0) {
+            cout << "FizzBuzz\n";
         }
-        if (i % 3 == 0) {
-            std::cout << "Fizz" << "\n";
-            continue;
+        else if (i % 5 == 0) {
+            cout << "Buzz\n";
         }
-        if (i % 5 == 0) {
-            std::cout << "Buzz" << "\n";
-            continue;
+        else if (i % 3 == 0) {
+            cout << "Fizz\n";
         }
-        std::cout << i << "\n";
+        else
+            cout << i << "\n";
     }
     return 0;
 }
